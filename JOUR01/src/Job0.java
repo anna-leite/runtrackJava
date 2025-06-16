bc14434f0fadf5f4401270bc9cb50bf029266d52
anna-leite <anna.leite@laplateforme.io>
2025-06-16 14:17:04 +0200

    Job0

---
+++ JOUR01/src/Job0.java
@@ -0,0 +1,26 @@
+
+
+public class Job0 {
+    public static void Variables() {
+        char var1 = 'A';
+        System.out.println("char variable : " + var1);
+
+        String var2 = "Hello world!";
+        System.out.println("string variable : " + var2);
+
+        int var3 = 2;
+        System.out.println("int variable : " + var3);
+
+        long var4 = 9223372036854775L;
+        System.out.println("long variable : " + var4);
+
+        float var5 = 1.2f;
+        System.out.println("float variable : " + var5);
+
+        boolean var6 =  true;
+        System.out.println("boolean variable : " + var6);
+
+        int TOTO = (int)3.817f;
+        System.out.println("TOTO variable : " + TOTO);
+    }
+}
