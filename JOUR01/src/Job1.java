26a40aed0ad5748040ba1d7ebb3a9121e19073da
anna-leite <anna.leite@laplateforme.io>
2025-06-16 14:30:25 +0200

    Job1

---
+++ JOUR01/src/Job1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Job1 {
+    public static void Print(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word : ");
+        String word = sc.nextLine();
+        System.out.println("The word is : " + word);
+    }
+}
