f36211f427c59e1b26c37521f43b4f799e6018e3
anna-leite <anna.leite@laplateforme.io>
2025-06-16 14:38:11 +0200

    Job2

---
+++ JOUR01/src/Job2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Job2 {
+    public static void PrintHelloName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first name : ");
+        String firstName = sc.nextLine();
+        System.out.println("Hello " +firstName +"!");
+    }
+}
--- JOUR01/src/Main.java
+++ JOUR01/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 //        Job0.Variables();
-        Job1.Print();
+//        Job1.Print();
+        Job2.PrintHelloName();
     }
 }
