dd32a9975da4cbfdc19550918768479dad73d8ef
anna-leite <anna.leite@laplateforme.io>
2025-06-17 10:55:59 +0200

    Job3

--- src/JOUR02/Toto.java
+++ src/JOUR02/Toto.java
@@ -2,16 +2,16 @@ package JOUR02;
 
 public class Toto {
     int toto = 0;
-//    static int toto =0;
+    static int titi =0;
     Toto() {
         toto = toto + 1;
     }
     public static void main(String[] tutu) {
         Toto t1 = new Toto();
         Toto t2 = new Toto();
-        System.out.println("Toto : " + toto);
-//        System.out.println("Toto t1 : " + t1.toto);
-//        System.out.println("Toto t2 : " + t2.toto);
+        System.out.println("Toto : " + titi);
+        System.out.println("Toto t1 : " + t1.toto);
+        System.out.println("Toto t2 : " + t2.toto);
     }
 }
 
