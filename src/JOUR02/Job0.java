38b558e3b270bc887fe1fca16f37fb4bfe02971b
anna-leite <anna.leite@laplateforme.io>
2025-06-17 10:26:22 +0200

    Job0

--- src/JOUR02/Job0.java
+++ src/JOUR02/Job0.java
@@ -1,4 +1,18 @@
 package JOUR02;
 
-public class Job0 {
+public class Personn {
+    public String firstName;
+    public String lastName;
+    protected int birthDay;
+    protected int birthMonth;
+    protected int birthYear;
+    protected String birthPlace;
+    private String adress;
+    private String telephoneNumber;
+
+    // public is accessible without conditions
+    // private is only accessible by the class itself
+    // protected is accessible also by the daughter classes
+
+    // need setters and getters for private and protected attributes
 }
--- src/JOUR02/Main.java
+++ src/JOUR02/Main.java
@@ -1,4 +1,7 @@
 package JOUR02;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
