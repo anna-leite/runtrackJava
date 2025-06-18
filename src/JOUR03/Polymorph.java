2a77f008b05f44605017efeb520e754d5b21d1c2
anna-leite <anna.leite@laplateforme.io>
2025-06-18 14:46:39 +0200

    Job3

---
+++ src/JOUR03/Polymorph.java
@@ -0,0 +1,89 @@
+package JOUR03;
+
+public class Polymorph {
+    public static void main(String[] args) {
+        Forme[] tabFormes = {
+                new Cercle("rouge"),
+                new Triangle("jaune")
+        };
+        Collect formes = new Collect(10);
+
+        for (int i = 0; i < tabFormes.length; i++) {
+            formes.add(tabFormes[i]);
+//            formes.add(new Forme(tabFormes[i]));
+            // si on appelle un constructeur pour faire la copie, alors le nouvel objet sera
+            // du type du constructeur donc Forme.
+        }
+        formes.dessine();
+    }
+}
+
+
+class Forme {
+    private String couleur;
+
+    public Forme(String uneCouleur) {
+        couleur = uneCouleur;
+    }
+
+    public Forme(Forme other) {
+        this.couleur = other.couleur;
+    }
+
+    public void dessine() {
+        System.out.println("une forme " + couleur);
+    }
+}
+
+class Triangle extends Forme {
+    public  Triangle(String uneCouleur) {
+        super(uneCouleur);
+    }
+
+    public Triangle(Triangle autreTriangle) {
+        super(autreTriangle);
+    }
+
+    public void dessine() {
+        super.dessine();
+        System.out.println("toute pointue");
+    }
+}
+
+class Cercle extends Forme {
+    public Cercle(String uneCouleur) {
+        super(uneCouleur);
+    }
+
+    public Cercle(Cercle autreCercle) {
+        super(autreCercle);
+    }
+
+    public void dessine() {
+        super.dessine();
+        System.out.println("toute ronde");
+    }
+}
+
+class Collect {
+    private Forme collect[];
+    private int index;
+
+    public Collect(int indexMax) {
+        collect = new Forme[indexMax];
+        index = -1;
+    }
+
+    public void add(Forme a) {
+        if (index < collect.length - 1) {
+            ++ index;
+            collect[index] = a;
+        }
+    }
+
+    public void dessine() {
+        for (int i = 0; i <= index; i++) {
+            collect[i].dessine();
+        }
+    }
+}
