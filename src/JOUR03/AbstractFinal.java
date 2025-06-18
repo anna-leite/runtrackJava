c32c61a3554e44d639f09025d3a710b182c78ce9
anna-leite <anna.leite@laplateforme.io>
2025-06-18 11:23:32 +0200

    Job1

---
+++ src/JOUR03/AbstractFinal.java
@@ -0,0 +1,34 @@
+package JOUR03;
+
+public class AbstractFinal {
+    public static void main(String[] args) {
+        A[] tab = new A[3];
+        tab[0] = new A(); // 'A' is abstract; cannot be instantiated
+        tab[1] = new B();
+        tab[2] = new C();
+        tab[1].b = 2; // tab[1] non convert into B objet
+        ((C)tab[2]).c = 3; // cannot change c because is a final type
+    }
+}
+
+abstract class A {
+    int a;
+}
+
+class B extends A {
+    int b;
+}
+
+class C extends A {
+    final double c = 1;
+}
+
+abstract class D extends A {
+    double d;
+
+    int operation(int a) {
+        return (a*2);
+    }
+
+    abstract int calcul(int b) {} // abstract methode cannot have a body
+}
---
+++ src/JOUR03/Main.java
@@ -0,0 +1,5 @@
+package JOUR03;
+
+public class Main {
+    public static void main(String[] args) {}
+}
